9b112b85774dfd45face866c82996eca7f39be39
k1n4kut4 <52294958+k1n4kut4@users.noreply.github.com>
2019-06-27 21:24:33 +0100

    fleshed out

--- csvMasher.py
+++ csvMasher.py
@@ -1,6 +1,23 @@
 import csv
 
-with open('ONSPD_FEB_2019_UK_AB.csv', newline='') as csvfile:
+with open('ONSPD_FEB_2019_UK.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+    post_code_abbrv_last = ""
+    i = 1
     for row in reader:
-        print(row["pcd"] + ", " + row["pcon"] + ", " + row["eer"] + ", " + row["rgn"])
+        #print(row["pcd"])
+        post_code_abbrv = row["pcd"][:-2].replace(" ", "")
+        #print(post_code_abbrv)
+        if post_code_abbrv != post_code_abbrv_last:
+            if row["pcon"] != "":
+                csvData = post_code_abbrv + ", " + row["pcon"] + ", " + row["eer"] + ", " + row["rgn"]
+                print(i)
+                print(csvData)
+                with open('ConstitPostCodes_abbrv.csv', 'a') as newFile:
+                    fieldnames = ['post_code', 'id_uk_constit', 'id_eu_constit', 'region']
+                    writer = csv.DictWriter(newFile, fieldnames=fieldnames)
+                    #writer.writeheader() 
+                    writer.writerows([{'post_code': post_code_abbrv, 'id_uk_constit': row["pcon"], 'id_eu_constit': row["eer"], 'region': row["rgn"]}])
+                print("Row written")
+                i = i + 1
+                post_code_abbrv_last = post_code_abbrv
